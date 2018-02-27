e3fd0f0f57138fb743bd3e0c41abe44c97d376c8
Dorin130 <dorin.gujuman@tecnico.ulisboa.pt>
2018-02-27 14:29:50 +0000

    auto-generated tax and car modules

---
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/IRS.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.tecnico.softeng.tax;
+
+/**
+ * Hello world!
+ *
+ */
+public class IRS 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
