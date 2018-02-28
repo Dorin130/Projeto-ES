00889c4d9fc8b58b003f911369ace0d8de40c122
JoaoPBSousa <joao.sousa9397@gmail.com>
2018-02-28 19:28:42 +0000

    closes #25

---
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/domain/Car.java
@@ -0,0 +1,14 @@
+package pt.ulisboa.tecnico.softeng.car.domain;
+
+import pt.ulisboa.tecnico.softeng.car.exception.VehicleException;
+
+//license plate test incomplete
+
+public class Car extends Vehicle{
+	
+	
+	public Car(String plate, int kilometers, RentACar rentAcar) {
+		super(plate, kilometers, rentAcar);
+		
+	}
+}
