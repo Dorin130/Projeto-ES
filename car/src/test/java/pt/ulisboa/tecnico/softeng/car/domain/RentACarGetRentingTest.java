ae5fcb9505ab6947eba2cef214b5c448c913750d
Nadia Sofia <nadia.sofia@tecnico.ulisboa.pt>
2018-03-01 15:50:24 +0000

    RentACarGetRenting Test done

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingTest.java
@@ -1,5 +1,10 @@
 package pt.ulisboa.tecnico.softeng.car.domain;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import org.joda.time.LocalDate;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,25 +13,37 @@ import org.junit.Before;
 import pt.ulisboa.tecnico.softeng.car.exception.CarException;
 
 public class RentACarGetRentingTest {
-	private static final String RENTING_REFERENCE = "ABC123";
+	private static final String DRIVING_LICENSE = "123456";
+	private final LocalDate begin = new LocalDate(2018, 01, 01);
+	private final LocalDate end = new LocalDate(2018, 01, 03);
 	private RentACar rentacar;
-	private Vehicle vehicle;
 	private Renting renting;
 	
 	@Before
 	public void setUp() {
 		this.rentacar = new RentACar("CompanyName");
-		this.vehicle = new Vehicle("AA-09-BB", 0, rentacar);
-		this.renting = new Renting(RENTING_REFERENCE, vehicle);
+		this.renting = this.rentacar.rent(this.rentacar, DRIVING_LICENSE, this.begin, this.end);
 	}
 	
 	@Test
 	public void sucess() {
-		Renting result = this.rentacar.getRenting(this.renting.getReference());
-	
-		Assert.assertEquals(RENTING_REFERENCE, this.renting.getReference());
-		Assert.assertEquals(this.renting, result);
+		Assert.assertEquals(this.renting, this.rentacar.getRenting(this.renting.getReference()));
 	}
 	
+	@Test
+	public void successCancelled() {
+		this.renting.cancel();
+		Assert.assertEquals(this.renting, this.rentacar.getRenting(this.renting.getCancellation()));
+	}
+	
+	@Test
+	public void doesNotExist() {
+		Assert.assertNull(this.rentacar.getRenting("123a"));
+	}
+
+	@After
+	public void tearDown() {
+		RentACar.rentacars.clear();
+	}
 	
 }
