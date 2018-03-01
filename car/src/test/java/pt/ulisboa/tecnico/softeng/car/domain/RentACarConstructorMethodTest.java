a644ddab10df000d5ec3a85ec6cb4016aa11eae5
Nadia Sofia <nadia.sofia@tecnico.ulisboa.pt>
2018-03-01 10:39:19 +0000

    started RentACar test

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarConstructorMethodTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarConstructorMethodTest.java
@@ -1,8 +1,12 @@
 package pt.ulisboa.tecnico.softeng.car.domain;
 
+import org.junit.After;
+import org.junit.Assert;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
 
 /**
  * Unit test for simple App.
@@ -10,6 +14,8 @@ import junit.framework.TestSuite;
 public class RentACarConstructorMethodTest 
     extends TestCase
 {
+	private static final String RENTACAR_NAME = "CompanyName";
+	
     /**
      * Create the test case
      *
@@ -25,14 +31,30 @@ public class RentACarConstructorMethodTest
      */
     public static Test suite()
     {
-        return new TestSuite( RentACarConstructorMethodTest.class );
+        return new TestSuite(RentACarConstructorMethodTest.class);
     }
+    
+    @Test
+    public void success() {
+		RentACar rentacar = new RentACar(RENTACAR_NAME);
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		Assert.assertEquals(RENTACAR_NAME, rentacar.getName());
+		Assert.assertEquals(1, RentACar.rentacars.size());
+	}
+
+	@Test(expected = RentACarException.class)
+	public void nullName() {
+		new RentACar(null);
+	}
+
+	@Test(expected = RentACarException.class)
+	public void emptyName() {
+		new RentACar("");
+	}
+	
+	@After
+	public void tearDown() {
+		RentACar.rentacars.clear();
+	}
+
 }
