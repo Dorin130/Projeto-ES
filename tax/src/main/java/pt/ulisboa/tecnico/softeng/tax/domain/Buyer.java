a8ad34888e8c307166f1255eda3d2c1a640b868f
hugosilvaguerreiro <hugo.guerreiro@tecnico.ulisboa.pt>
2018-03-04 22:21:39 +0000

    Buyer.taxReturn implemented

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Buyer.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Buyer.java
@@ -1,5 +1,7 @@
 package pt.ulisboa.tecnico.softeng.tax.domain;
 
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
+
 public class Buyer extends TaxPayer {
 
 	public Buyer(String NIF, String name, String address) {
@@ -8,7 +10,13 @@ public class Buyer extends TaxPayer {
 	}
 
 	float taxReturn(int YEAR) {
-    //TODO: implement
+    	if( YEAR < 1970)
+    		throw new TaxException();
+    	float taxReturn = 0;
+    	for(Invoice i : this.getInvoices()) {
+    		taxReturn += i.getIVA()*0.05;
+		}
+		return taxReturn;
 	}
 
 }
