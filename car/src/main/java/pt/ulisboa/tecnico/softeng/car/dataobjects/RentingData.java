b0ca6e4bb285ac63be80ede118ef4f265ebea13d
Nadia Sofia <nadia.sofia@tecnico.ulisboa.pt>
2018-03-06 16:49:45 +0000

    Finished RentingData Class, closes #41

--- car/src/main/java/pt/ulisboa/tecnico/softeng/car/dataobjects/RentingData.java
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/dataobjects/RentingData.java
@@ -2,12 +2,70 @@ package pt.ulisboa.tecnico.softeng.car.dataobjects;
 
 import org.joda.time.LocalDate;
 
-//import pt.ulisboa.tecnico.softeng.car.exception.CarException;
-
 public class RentingData {
+	private String reference;
+	private String plate;
+	private String drivingLicense;
+	private String rentACarCode;
+	private LocalDate begin;
+	private LocalDate end;
 	
-	public RentingData(String reference, String plate, String drivingLicense,
-			String rentACarCode, LocalDate begin, LocalDate end) {
-		
+	public RentingData(String reference, String plate, String drivingLicense, String rentACarCode, LocalDate begin, LocalDate end) {
+		this.setReference(reference);
+		this.setPlate(plate);
+		this.setDrivingLicense(drivingLicense);
+		this.setRentACarCode(rentACarCode);
+		this.setBegin(begin);
+		this.setEnd(end);
 	}
+
+	public String getReference() {
+		return this.reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	public String getPlate() {
+		return this.plate;
+	}
+
+	public void setPlate(String plate) {
+		this.plate = plate;
+	}
+
+	public String getDrivingLicense() {
+		return this.drivingLicense;
+	}
+
+	public void setDrivingLicense(String drivingLicense) {
+		this.drivingLicense = drivingLicense;
+	}
+
+	public String getRentACarCode() {
+		return this.rentACarCode;
+	}
+
+	public void setRentACarCode(String rentACarCode) {
+		this.rentACarCode = rentACarCode;
+	}
+
+	public LocalDate getBegin() {
+		return this.begin;
+	}
+
+	public void setBegin(LocalDate begin) {
+		this.begin = begin;
+	}
+
+	public LocalDate getEnd() {
+		return this.end;
+	}
+
+	public void setEnd(LocalDate end) {
+		this.end = end;
+	}
+	
+	
 }
