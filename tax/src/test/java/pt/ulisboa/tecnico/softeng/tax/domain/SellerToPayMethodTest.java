63c74b04e8edb61bba76a1bbbfb8c8b98375c576
Marco Silva <marcofsilva@tecnico.ulisboa.pt>
2018-03-06 00:38:56 +0000

    Created files for testing Seller methods

---
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/SellerToPayMethodTest.java
@@ -0,0 +1,24 @@
+package pt.ulisboa.tecnico.softeng.tax.domain;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SellerToPayMethodTest {
+	
+	
+	@Before
+	public void setUp() {
+		//TODO
+	}
+	
+	@Test
+	public void something() {
+		//TODO
+	}
+	
+	@After
+	public void tearDown() {
+		//TODO
+	}
+}
