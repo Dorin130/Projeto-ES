56c122ce9c60248ec643fa0590483c5771689c6c
Hugo Guerreiro <hugo.guerreiro@tecnico.ulisboa.pt>
2018-03-06 11:45:43 +0000

    fixed minor errors in bankprocesspayment test class; fixed minor errors in hotel reserve room test class; fixed minor error in itemType class

--- bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankProcessPaymentMethodTest.java
+++ bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankProcessPaymentMethodTest.java
@@ -28,8 +28,8 @@ public class BankProcessPaymentMethodTest {
         Client client = new Client(bank, "Manuel");
         Account account = new Account(bank, client);
         account.deposit(100);
-        String reference1 = this.bank.processPayment(this.account.getIBAN(), 10);
-        String reference2 = bank.processPayment(account.getIBAN(), 10);
+        String reference1 = Bank.processPayment(this.account.getIBAN(), 10);
+        String reference2 = Bank.processPayment(account.getIBAN(), 10);
 
         Operation operation1 = this.bank.getOperation(reference1);
         Assert.assertNotNull(operation1);
@@ -37,7 +37,7 @@ public class BankProcessPaymentMethodTest {
         Assert.assertEquals(this.account.getBalance(), 90);
 
 
-        Operation operation2 = this.bank.getOperation(reference2);
+        Operation operation2 = bank.getOperation(reference2);
         Assert.assertNotNull(operation2);
         Assert.assertEquals(account, operation2.getAccount());
         Assert.assertEquals(account.getBalance(), 90);
@@ -46,46 +46,46 @@ public class BankProcessPaymentMethodTest {
 
     @Test(expected = BankException.class)
     public void inexistentAccount() {
-        this.bank.processPayment("BK021", 10);
+        Bank.processPayment("BK021", 10);
     }
 
     @Test
     public void multipleAccountsInTheBank() {
         Client client = new Client(this.bank, "Manuel");
         new Account(this.bank, client);
-        this.bank.processPayment(this.account.getIBAN(), 10);
+        Bank.processPayment(this.account.getIBAN(), 10);
         Assert.assertEquals(90, this.account.getBalance());
     }
 
 
     @Test(expected = BankException.class)
     public void nullAmount() {
-        this.bank.processPayment(this.account.getIBAN(), 10);
+        Bank.processPayment(this.account.getIBAN(), 10);
     }
 
     @Test(expected = BankException.class)
     public void zeroAmount() {
-        this.bank.processPayment(this.account.getIBAN(), 0);
+        Bank.processPayment(this.account.getIBAN(), 0);
     }
 
     @Test
     public void oneAmount() {
-        this.bank.processPayment(this.account.getIBAN(), 1);
+        Bank.processPayment(this.account.getIBAN(), 1);
     }
 
     @Test(expected = BankException.class)
     public void notEnoughBalance() {
-        this.bank.processPayment(this.account.getIBAN(), this.account.getBalance() + 1);
+        Bank.processPayment(this.account.getIBAN(), this.account.getBalance() + 1);
     }
 
     @Test
     public void allTheBalance() {
-        this.bank.processPayment(this.account.getIBAN(), this.account.getBalance());
+        Bank.processPayment(this.account.getIBAN(), this.account.getBalance());
     }
 
     @Test(expected = BankException.class)
     public void negativeAmount() {
-        this.bank.processPayment(this.account.getIBAN(), -100);
+        Bank.processPayment(this.account.getIBAN(), -100);
     }
 
 
