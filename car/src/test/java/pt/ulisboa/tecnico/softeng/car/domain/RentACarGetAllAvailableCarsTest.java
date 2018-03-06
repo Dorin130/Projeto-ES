2a533e528cd04a5c66123d7ebfc3fb684cb03a20
Nadia Sofia <nadia.sofia@tecnico.ulisboa.pt>
2018-03-06 16:46:26 +0000

    Finished RentACar GetAllAvailableCars Test, closes #3

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetAllAvailableCarsTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetAllAvailableCarsTest.java
@@ -1,8 +1,8 @@
 package pt.ulisboa.tecnico.softeng.car.domain;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.joda.time.LocalDate;
@@ -13,14 +13,36 @@ import org.junit.Test;
 public class RentACarGetAllAvailableCarsTest {
 	private final LocalDate begin = new LocalDate(2018, 01, 01);
 	private final LocalDate end = new LocalDate(2018, 01, 03);
+	private final Set<Car> availableCars = new HashSet<>();
 	private RentACar rentacar;
+	private RentACar extraRentacar;
 	
 	@Before
-	public void setUp() {
+	public void setUp() {		
 		this.rentacar = new RentACar("CompanyName");
-		new Car("AA-01-BB", 400, this.rentacar);
-		new Car("CC-02-DD", 300, this.rentacar);
-		new Car("EE-03-FF", 200, this.rentacar);
-		new Car("GG-04-HH", 100, this.rentacar);
+		this.extraRentacar = new RentACar("CompanyNameExtra");
+		new Motorcycle("AA-01-BB", 400, this.rentacar);
+		availableCars.add(new Car("CC-02-DD", 300, this.rentacar));
+		availableCars.add(new Car("EE-03-FF", 200, this.rentacar));
+		availableCars.add(new Car("GG-04-HH", 100, this.rentacar));
 	}
+	
+	@Test
+	public void success() {
+		Set<Car> cars = RentACar.getAllAvailableCars(begin, end);
+		assertEquals(availableCars, cars);
+	}
+	
+	@Test
+	public void multipleRentACars() {
+		availableCars.add(new Car("II-05-JJ", 1500, extraRentacar));
+		
+		Set<Car> cars = RentACar.getAllAvailableCars(begin, end);
+		assertEquals(availableCars, cars);
+	}
+	
+	@After
+	public void tearDown() {
+		RentACar.rentacars.clear();
+	}	
 }
