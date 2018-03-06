f4d2df2a8eca17e698d0ec19c8788e84a505903c
Dorin130 <dorin.gujuman@tecnico.ulisboa.pt>
2018-03-06 11:09:42 +0000

    Add addInvoice to ItemType

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/ItemType.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/ItemType.java
@@ -1,14 +1,21 @@
 package pt.ulisboa.tecnico.softeng.tax.domain;
 
+import java.util.HashSet;
+
 public class ItemType {
 
 
 	private String name;
 	private double tax;
+	private HashSet<Invoice> invoices = new HashSet<Invoice>();
 	
 	public ItemType(String name, double tax) {
 		this.name = name;
 		this.tax = tax;
+	}
+
+	public void addInvoice(Invoice invoice) {
+		invoices.add(invoice);
 	}
 
 	public String getName() {
