0f3e2eb11bf4b074999f32ba7c4163c9a84919c7
Marco Silva <marcofsilva@tecnico.ulisboa.pt>
2018-03-07 00:15:33 +0000

    this closes #21

--- tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/SellerGetInvoiceByReferenceMethodTest.java
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/SellerGetInvoiceByReferenceMethodTest.java
@@ -1,24 +1,65 @@
 package pt.ulisboa.tecnico.softeng.tax.domain;
 
+import org.joda.time.LocalDate;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
+
 public class SellerGetInvoiceByReferenceMethodTest {
 	
+	private static final float VALUE = 10.5f;
+    private static final LocalDate DATE = new LocalDate(2010, 10, 1);
+    private static final String ITEM_TYPE = "vinho";
+    private static final String SELLER = "123456789";
+    private static final String BUYER = "456123789";
+	
+	//invoice_reference from count in Invoice, so it's value is 1
+	private static final String INVOICE_REFERENCE = "1";
+	private IRS irs;
+	private Seller seller;
 	
 	@Before
 	public void setUp() {
-		//TODO
+		irs = IRS.getInstance();
+		seller = new Seller("123456789", "Marco", "Parchal");
+		new ItemType(ITEM_TYPE, 50);
 	}
 	
 	@Test
-	public void something() {
-		//TODO
+	public void success() {
+		seller.addInvoice(new Invoice(VALUE, DATE, ITEM_TYPE, SELLER, BUYER));
+		
+		Invoice invoice = this.seller.getInvoiceByReference(INVOICE_REFERENCE);
+		Assert.assertNotNull(invoice);
 	}
 	
+	@Test
+	public void ReturnNull() {
+		Invoice invoice = this.seller.getInvoiceByReference("2");
+		Assert.assertNull(invoice);
+	}
+	
+	@Test(expected = TaxException.class)
+	public void nullInvoiceReference() {
+		seller.getInvoiceByReference(null);
+	}
+	
+	@Test(expected = TaxException.class)
+	public void emptyInvoiceReference() {
+		seller.getInvoiceByReference("");
+	}
+	
+	@Test(expected = TaxException.class)
+	public void blankReference() {
+		seller.getInvoiceByReference("   ");
+	}
+		
 	@After
 	public void tearDown() {
-		//TODO
+		this.irs.clearTaxPayers();
+		this.irs.clearItemTypes();
 	}
 }
