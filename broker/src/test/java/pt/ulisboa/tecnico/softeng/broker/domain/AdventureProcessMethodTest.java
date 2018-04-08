ddce8a6e24ebc4f3fbbc0c08262d42a709d2bb52
Hugo Guerreiro <hugo.guerreiro@tecnico.ulisboa.pt>
2018-04-08 16:26:28 +0100

    fixed adventureProcess test

--- broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/AdventureProcessMethodTest.java
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/AdventureProcessMethodTest.java
@@ -3,26 +3,40 @@ package pt.ulisboa.tecnico.softeng.broker.domain;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
 import org.joda.time.LocalDate;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.junit.runner.RunWith;
 import pt.ulisboa.tecnico.softeng.activity.domain.Activity;
 import pt.ulisboa.tecnico.softeng.activity.domain.ActivityOffer;
 import pt.ulisboa.tecnico.softeng.activity.domain.ActivityProvider;
 import pt.ulisboa.tecnico.softeng.bank.domain.Account;
 import pt.ulisboa.tecnico.softeng.bank.domain.Bank;
 import pt.ulisboa.tecnico.softeng.bank.domain.Client;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.CarInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.TaxInterface;
 import pt.ulisboa.tecnico.softeng.hotel.domain.Hotel;
 import pt.ulisboa.tecnico.softeng.hotel.domain.Room;
 import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type;
 
+@RunWith(JMockit.class)
 public class AdventureProcessMethodTest {
 	private static final String HOTEL_NAME = "Londres";
 	private static final String HOTEL_CODE = "XPTO123";
 	private static final String NIF = "123456789";
-
+	private static final int AMOUNT = 200;
+	private static final boolean NOT_RENT_CAR = false;
+	private static final String ROOM_CONFIRMATION = "RoomConfirmation";
+	private static final String VEHICLE_REFERENCE = "vehicleReference";
+	private static final String PAYMENT_CONFIRMATION = "PaymentConfirmation";
+	private static final String TAX_CONFIRMATION = "TaxConfirmation";
 	private static final String DRIVING_LICENSE = "IMT1234";
 	private final int priceSingle = 50;
 	private final int priceDouble = 100;
@@ -56,8 +70,24 @@ public class AdventureProcessMethodTest {
 	}
 
 	@Test
-	public void success() {
-		Adventure adventure = new Adventure(this.broker, this.begin, this.end, this.clientBroker, 300, true);
+	public void success(@Mocked BankInterface bankInterface, @Mocked TaxInterface taxInterface,
+						@Mocked CarInterface carInterface, @Mocked HotelInterface hotelInterface) {
+		new Expectations() {
+			{
+				HotelInterface.reserveRoom(Type.SINGLE, begin, end, buyerNIF, IBAN);
+				this.result = ROOM_CONFIRMATION;
+
+				CarInterface.processVehicleRenting(DRIVING_LICENSE, begin, end, NIF, IBAN);
+				this.result = VEHICLE_REFERENCE;
+
+				BankInterface.processPayment(IBAN, AMOUNT);
+				this.result = PAYMENT_CONFIRMATION;
+
+				TaxInterface.submitInvoice(null, null, anyString, AMOUNT, new LocalDate());
+				this.result =  TAX_CONFIRMATION;
+			}
+		};
+		Adventure adventure = new Adventure(this.broker, this.begin, this.end, this.clientBroker, AMOUNT, true);
 
 		adventure.process();
 		adventure.process();
@@ -72,8 +102,21 @@ public class AdventureProcessMethodTest {
 	}
 
 	@Test
-	public void successNoHotelBooking() {
-		Adventure adventure = new Adventure(this.broker, this.begin, this.begin, this.clientBroker, 300, true);
+	public void successNoHotelBooking(@Mocked BankInterface bankInterface, @Mocked TaxInterface taxInterface,
+									  @Mocked CarInterface carInterface) {
+		new Expectations() {
+			{
+				CarInterface.processVehicleRenting(DRIVING_LICENSE, begin, begin, NIF, IBAN);
+				this.result = VEHICLE_REFERENCE;
+
+				BankInterface.processPayment(IBAN, AMOUNT);
+				this.result = PAYMENT_CONFIRMATION;
+
+				TaxInterface.submitInvoice(null, null, anyString, AMOUNT, new LocalDate());
+				this.result =  TAX_CONFIRMATION;
+			}
+		};
+		Adventure adventure = new Adventure(this.broker, this.begin, this.begin, this.clientBroker, AMOUNT, true);
 
 		adventure.process();
 		adventure.process();
@@ -86,27 +129,49 @@ public class AdventureProcessMethodTest {
 	}
 	
 	@Test
-	public void successNoVehicleBooking() {
-		Adventure adventure = new Adventure(this.broker, this.begin, this.end, this.clientBroker, 300, false);
+	public void successNoVehicleBooking(@Mocked BankInterface bankInterface, @Mocked TaxInterface taxInterface,
+										@Mocked HotelInterface hotelInterface) {
+		new Expectations() {
+			{
+				HotelInterface.reserveRoom(Type.SINGLE, begin, end, buyerNIF, IBAN);
+				this.result = ROOM_CONFIRMATION;
+
+				BankInterface.processPayment(IBAN, AMOUNT);
+				this.result = PAYMENT_CONFIRMATION;
+
+				TaxInterface.submitInvoice(null, null, anyString, AMOUNT, new LocalDate());
+				this.result =  TAX_CONFIRMATION;
+			}
+		};
+		Adventure adventure = new Adventure(this.broker, this.begin, this.end, this.clientBroker, AMOUNT, false);
 
 		adventure.process();
 		adventure.process();
 		adventure.process();
 
-		//assertEquals(Adventure.State.CONFIRMED, adventure.getState());
-		assertNotNull(adventure.getActivityConfirmation());
-		assertNotNull(adventure.getVehicleConfirmation());
-		assertNotNull(adventure.getPaymentConfirmation());
+		assertEquals(Adventure.State.CONFIRMED, adventure.getState());
+		//assertNotNull(adventure.getActivityConfirmation());
+		//assertNotNull(adventure.getVehicleConfirmation());
+		//assertNotNull(adventure.getPaymentConfirmation());
 	}
 	
 	@Test
-	public void successNoRoomNoVehicleBooking() {
-		Adventure adventure = new Adventure(this.broker, this.begin, this.begin, this.clientBroker, 300, true);
+	public void successNoRoomNoVehicleBooking(@Mocked BankInterface bankInterface, @Mocked TaxInterface taxInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(IBAN, 200);
+				this.result = PAYMENT_CONFIRMATION;
+
+				TaxInterface.submitInvoice(null, null, anyString, AMOUNT, new LocalDate());
+				this.result =  TAX_CONFIRMATION;
+			}
+		};
+		Adventure adventure = new Adventure(this.broker, this.begin, this.begin, this.clientBroker, AMOUNT, false);
 
 		adventure.process();
 		adventure.process();
 
-		//assertEquals(Adventure.State.CONFIRMED, adventure.getState());
+		assertEquals(Adventure.State.CONFIRMED, adventure.getState());
 		assertNotNull(adventure.getActivityConfirmation());
 		assertNotNull(adventure.getPaymentConfirmation());
 	}
