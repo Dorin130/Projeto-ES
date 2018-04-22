0ff2e4dfebac878b87a21e867a03ebc290a7b6ca
João <36603746+JoaoPBSousa@users.noreply.github.com>
2018-04-22 11:43:19 +0100

    Issue122 (#140)
    closes #122

--- tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/TaxPersistenceTest.java
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/TaxPersistenceTest.java
@@ -2,17 +2,23 @@ package pt.ulisboa.tecnico.softeng.tax.domain;
 
 import static org.junit.Assert.assertEquals;
 
+
 import java.util.HashSet;
 import java.util.Set;
+
 
 import org.junit.After;
 import org.junit.Test;
 
 import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
 import pt.ist.fenixframework.Atomic.TxMode;
 
 
 public class TaxPersistenceTest {
+
+	private static final String ITEMTYPE_NAME = "XPTO";
+	private static final int ITEMTYPE_TAX = 2;
 	private static final String NIF_BUYER = "123456789";
 	private static final String ADDRESS_BUYER = "address";
 	private static final String NAME_BUYER = "João";
@@ -31,6 +37,8 @@ public class TaxPersistenceTest {
 	@Atomic(mode = TxMode.WRITE)
 	public void atomicProcess() {
 		IRS irs = IRS.getIRS();
+
+		ItemType itemtype = new ItemType(irs, ITEMTYPE_NAME, ITEMTYPE_TAX);
 		
 		taxPayersAdded.add(new Seller(irs, NIF_SELLER, NAME_SELLER, ADDRESS_SELLER));
 		taxPayersAdded.add(new Buyer(irs, NIF_BUYER, NAME_BUYER, ADDRESS_BUYER));
@@ -44,6 +52,9 @@ public class TaxPersistenceTest {
 		assertEquals(2, taxPayers.size());
 		assertEquals(taxPayersAdded, taxPayers);
 		
+		assertEquals(ITEMTYPE_NAME, irs.getItemTypeByName(ITEMTYPE_NAME).getName());
+		assertEquals(ITEMTYPE_TAX, irs.getItemTypeByName(ITEMTYPE_NAME).getTax());
+			
 	}
 	
 	@After
